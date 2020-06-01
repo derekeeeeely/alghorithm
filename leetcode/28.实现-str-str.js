b99f15e2b19754d7a5374cea713ecd30dd4e68dc
小孩别放肆 <derekely1@gmail.com>
2020-06-01 17:51:46 +0800

    2020.06.01

--- leetcode/28.实现-str-str.js
+++ leetcode/28.实现-str-str.js
@@ -11,6 +11,46 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+  if (!needle) { return 0 }
+
+  var generateNextArr = (str) => {
+    var y = 0;
+    var x = 1;
+    var next = new Array(str.length).fill(0);
+    while (x < str.length) {
+      if (str[x] == str[y]) {
+        y++;
+        next[x] = y;
+        x++;
+      } else if (y > 0) {
+        y = y - 1;
+      } else {
+        next[x] = 0;
+        x++;
+      }
+    }
+    return next;
+  }
+
+  var i = 0; // string
+  var j = 0; // pattern
+  var next = generateNextArr(needle)
+  while (i < haystack.length && j < needle.length) {
+    if (haystack[i] == needle[j]) {
+      j++
+      i++
+    } else if (j > 0) {
+      j = next[j - 1]
+    } else {
+      i++
+    }
+  }
+  if (j == needle.length) {
+    return i - j
+  } else {
+    return -1
+  }
+
 
 };
 // @lc code=end
